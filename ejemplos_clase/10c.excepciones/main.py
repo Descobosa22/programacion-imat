f871e522f6e2563e348b7e782f922e82e837cf8c
David Contreras <davidcb@comillas.edu>
2022-11-11 16:32:43 +0100

    Ejemplos de clase Excepciones - 10-nov

--- ejemplos_clase/10c.excepciones/main.py
+++ ejemplos_clase/10c.excepciones/main.py
@@ -1,15 +1,12 @@
 import funciones as func
 import errores as err
 
-try:
-    edad = func.pedir_edad()
-    print(edad)
-except err.EdadError as error:
-    print(error)
-
-
-
-
+if __name__ == "__main__":
+    try:
+        edad = func.pedir_edad()
+        print(edad)
+    except err.EdadError as error:
+        print(error)
 
 """nombre = func.pedir_nombre()
 persona = crear_persona(nombre, edad)
